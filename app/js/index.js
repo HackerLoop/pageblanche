5c5acbad2f348b9387a9d2d87dd379a320144d90
raph <raphael.kammoun@gmail.com>
2017-03-10 13:34:15 +0100

    remove alerts + fix indentation

--- app/js/index.js
+++ app/js/index.js
@@ -18,44 +18,44 @@ document.getElementById('save-changes').addEventListener('click',function(){
 },false);
 
 
-    document.getElementById('create-new-file').addEventListener('click',function(){
-        var content = document.getElementById("session-content").value;
+document.getElementById('create-new-file').addEventListener('click',function(){
+    var content = document.getElementById("session-content").value;
 
-        dialog.showSaveDialog(function (fileName) {
-            if (fileName === undefined){
-                console.log("You didn't save the file");
-                return;
-            }
+    dialog.showSaveDialog(function (fileName) {
+        if (fileName === undefined){
+            console.log("You didn't save the file");
+            return;
+        }
 
-            fs.writeFile(fileName, content, function (err) {
-                if(err){
-                    alert("An error ocurred creating the file "+ err.message)
-                }
-
-                alert("The file has been succesfully saved");
-            });
-        });
-    },false);
-
-    function readFile(filepath) {
-        fs.readFile(filepath, 'utf-8', function (err, data) {
+        fs.writeFile(fileName, content, function (err) {
             if(err){
-                alert("An error ocurred reading the file :" + err.message);
-                return;
+                console.log("An error ocurred creating the file "+ err.message)
             }
 
-            document.getElementById("session-content").value = data;
+            console.log("The file has been succesfully saved");
         });
-    }
+    });
+},false);
 
-    function saveChanges(filepath,content){
-        fs.writeFile(filepath, content, function (err) {
-            if(err){
-                alert("An error ocurred updating the file"+ err.message);
-                console.log(err);
-                return;
-            }
+function readFile(filepath) {
+    fs.readFile(filepath, 'utf-8', function (err, data) {
+        if(err){
+            console.log("An error ocurred reading the file :" + err.message);
+            return;
+        }
 
-            alert("The file has been succesfully saved");
-        });
-    }
+        document.getElementById("session-content").value = data;
+    });
+}
+
+function saveChanges(filepath,content){
+    fs.writeFile(filepath, content, function (err) {
+        if(err){
+            console.log("An error ocurred updating the file"+ err.message);
+            console.log(err);
+            return;
+        }
+
+        console.log("The file has been succesfully saved");
+    });
+}
